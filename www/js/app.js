d5969dc0e4f2a1d1ecf983ddaac4b8cfd80002d7
giulianoiacobelli <giuliano.iacobelli@gmail.com>
2014-10-07 13:03:56 +0200

    Update app.js

--- www/js/app.js
+++ www/js/app.js
@@ -21,7 +21,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
 })
 .constant('APPID', '')
 .constant('APIKEY','')
-.constant('BASEURL', '')
+.constant('BASEURL', '') // e.g. http://APPID.stamplay.com
 .config(function($stateProvider, $urlRouterProvider) {
   // Ionic uses AngularUI Router which uses the concept of states
   // Learn more here: https://github.com/angular-ui/ui-router
