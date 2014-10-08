201aa93a7184ba1647f20742ec4da8615173a8d7
Cristian Roselli <cristian.roselli@stamplay.com>
2014-10-08 10:26:11 +0200

    Added ng-cordova

--- www/js/app.js
+++ www/js/app.js
@@ -5,17 +5,19 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'starter.templatesComponent'])
-.run(function($ionicPlatform) {
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'starter.templatesComponent', 'ngCordova'])
+.run(function($ionicPlatform, $cordovaStatusbar) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
+
     if(window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
     if(window.StatusBar) {
       // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
+      $cordovaStatusbar.styleColor('white');
+      //StatusBar.styleDefault();
     }
   });
 })
